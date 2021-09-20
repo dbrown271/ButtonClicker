381646342c3b53a935b702f98a3e3f7b6f064463
dbrown271 <d.brown271@icloud.com>
2021-09-20 16:23:03 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function log(button) {
+    button.innerHTML = "Logout"
+    button.style.backgroundColor = "blue"
+}
+
+function disappear(element) {
+    element.remove()
+}
+
+function proof() {
+    console.log("Ninja Was Liked!")
+}
